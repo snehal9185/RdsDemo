2d43ca41272c21293e652133dbaf844fb7aa1b0d
snehal9185 <63599150+snehal9185@users.noreply.github.com>
2021-05-27 19:38:47 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,12 +1,12 @@
 
 variable "region" {
   type    = string
-  default = "us-east-2"
+  default = "eu-west-2"
 }
 variable "ami_id" {
   type = map
   default = {
-    us-east-2   = "ami-035b3c7efe6d061d5"
+    us-east-1   = "ami-035b3c7efe6d061d5"
     eu-west-2    = "ami-132b3c7efe6sdfdsfd"
     eu-central-1 = "ami-9787h5h6nsn75gd33"
   }
